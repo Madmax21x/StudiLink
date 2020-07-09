a38316d64f6fdff62a5e45b6107ea2fbadca0e8a
Julietta <jandreasyan@gmail.com>
2020-07-09 20:06:11 +0200

    prints

--- lib/design_course/models/http.dart
+++ lib/design_course/models/http.dart
@@ -11,23 +11,27 @@ class RequestResult {
 }
 
 Future<RequestResult> http_get(String route, [dynamic data]) async {
-  var url = "http://studilink.online/cours";
+  var url = "http://192.168.1.50/cours";
   var result = await http.get(url);
   return RequestResult(true, jsonDecode(result.body));
 }
 
 Future<RequestResult> http_post(String route, [dynamic data]) async {
-  var url = "http://studilink.online/$route";
+  var url = "http://192.168.1.50/$route";
+  print("data print :");
+  debugPrint(data.toString());
   var dataStr = jsonEncode(data);
   var result = await http
       .post(url, body: dataStr, headers: {"Content-Type": "application/json"});
+  debugPrint("on est ici 1 ==========");
+  print(result.body);
   return RequestResult(true, jsonDecode(result.body));
 }
 
 /// a modifier
 Future<bool> deleteCours(String id) async {
   final http.Response response = await http.delete(
-    'http://studilink.online/cours/$id',
+    'http://192.168.1.50/cours/$id',
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
